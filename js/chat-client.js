3e1129e6c4a4552ae99e0427786ae57e17f6a6dc
wzoe <455391638@qq.com>
2020-11-15 18:02:19 -0600

    basic skeleton

---
+++ js/chat-client.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $("#logInModal").modal("show")
+    $("#send").click(sendMessage)
+})
+
+// log in modal
+$("#logInModalSubmit").click(function () {
+    $("#logInModal").modal("toggle")
+})
+
+$("#createRoom").click(function () {
+    $("#logInModal").modal("show")
+})
+
+var socketio = io.connect();
+socketio.on("message_to_client",function(data) {
+    //Append an HR thematic break and the escaped HTML of the new message
+    $("#chatLog").append(`
+                    <div class="msg msg-self rounded">
+                    <div class="col-10">
+                        <b>USERNAME</b>
+                        <p>${data['message']}</p>
+                    </div>
+                    <div class="col-2"><img class="avatar" src="/img/avatar-1.png"/></div>
+                </div>`)
+});
+
+function sendMessage(){
+    var msg = document.getElementById("message_input").value;
+    socketio.emit("message_to_server", {message:msg});
+}
+
